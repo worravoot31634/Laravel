378a31315e50be12d021142ee08b6e15366d3b2e
Phongsachat <55147988+Phongsachat@users.noreply.github.com>
2020-01-06 03:15:09 +0700

    update V999999

--- resources/views/homeOrganization.php
+++ resources/views/homeOrganization.php
@@ -60,31 +60,9 @@
 
     <!--<img src="./Images/headerIndex.png" alt="boat " style="width:100%;min-height:350px;max-height:600px; ">-->
     <div class="w3-container w3-display-bottomleft w3-margin-bottom ">
-      <button onclick="document.getElementById( 'id01').style.display='block' "
-        class="w3-button w3-xlarge w3-theme w3-hover-teal ">บริจาคให้น้อน</button>
+      <a href="donateOrganization"><button class="w3-button w3-xlarge w3-theme w3-hover-teal ">บริจาคให้น้อน</button>
     </div>
   </div>
-
-  <!-- Modal -->
-  <div id="id01" class="w3-modal">
-    <div class="w3-modal-content w3-card-4 w3-animate-top ">
-      <header class="w3-container w3-teal w3-display-container ">
-        <span onclick="document.getElementById( 'id01').style.display='none' "
-          class="w3-button w3-teal w3-display-topright "><i class="fa fa-remove "></i></span>
-        <h4>Oh snap! We just showed you a modal..</h4>
-        <h5>Because we can <i class="fa fa-smile-o "></i></h5>
-      </header>
-      <div class="w3-container ">
-        <p>Cool huh? Ok, enough teasing around..</p>
-        <p>Go to our <a class="w3-text-teal " href="/w3css/default.asp ">W3.CSS Tutorial</a> to learn more!</p>
-      </div>
-      <footer class="w3-container w3-teal ">
-        <p>Modal footer</p>
-      </footer>
-    </div>
-  </div>
-
-
 
   <!-- Team Container -->
 
